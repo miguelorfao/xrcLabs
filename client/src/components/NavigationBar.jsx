724c4dddeb4f86bc631212bc77f2f59f63f07a92
miguelorfao <69687296+miguelorfao@users.noreply.github.com>
2024-01-29 14:32:16 +0000

    commit changes

--- client/src/components/NavigationBar.jsx
+++ client/src/components/NavigationBar.jsx
@@ -5,7 +5,7 @@ import SignOut from "./SignOut";
 function NavigationBar() {
   return (
     <div>
-      <nav className="navbar navbar-expand-lg  bg-dark">
+      <nav className="navbar navbar-expand-lg bg-dark">
         <div className="container-fluid">
           <button
             class="navbar-toggler"
@@ -19,7 +19,7 @@ function NavigationBar() {
             <span class="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-5  mb-2 mb-lg-0 w-100 d-flex justify-content-end align-items-center gap-3">
+            <ul className="navbar-nav me-5  mb-2 mb-lg-0">
               <li className="nav-item">
                 <Link to="/AdminDashboard" className="text-info">
                   {" "}
