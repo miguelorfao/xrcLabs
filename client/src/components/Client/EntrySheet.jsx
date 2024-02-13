41ec935f894b20bfabbc48367b8b4a77b78541ee
Emmanuel <nkememmanuel5505@gmail.com>
2024-02-13 20:37:49 +0100

    entrysheet

--- client/src/components/Client/EntrySheet.jsx
+++ client/src/components/Client/EntrySheet.jsx
@@ -4,6 +4,7 @@ import React, { useState } from "react";
 import SideNav from "../SideNav";
 import Header from "../Header";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Calendar from "../../images/Calendar.png";
 
 import {
   // fa-solid,
@@ -39,34 +40,70 @@ function CollabSheet() {
           <div className="flex space-x-4">
             <input type="checkbox" className="h-5 w-8"/>
             <p>Date</p>
-            <FontAwesomeIcon icon={faCaretDown} />
+            <FontAwesomeIcon className="cursor-pointer" icon={faCaretDown} />
           </div>
           <div className="flex space-x-4">
             {/* <input type="checkbox" className="h-5 w-8"/> */}
             <p>Name</p>
-            <FontAwesomeIcon icon={faCaretDown} />
+            <FontAwesomeIcon className="cursor-pointer" icon={faCaretDown} />
           </div>
           <div className="flex space-x-4">
             {/* <input type="checkbox" className="h-5 w-8"/> */}
             <p>Spots</p>
-            <FontAwesomeIcon icon={faCaretDown} />
+            <FontAwesomeIcon className="cursor-pointer" icon={faCaretDown} />
           </div>
           <div className="flex space-x-4">
             {/* <input type="checkbox" className="h-5 w-8"/> */}
             <p>Entries</p>
-            <FontAwesomeIcon icon={faCaretDown} />
+            <FontAwesomeIcon className="cursor-pointer" icon={faCaretDown} />
           </div>
           <div className="flex space-x-4">
             {/* <input type="checkbox" className="h-5 w-8"/> */}
             <p>Platform</p>
-            <FontAwesomeIcon icon={faCaretDown} />
+            <FontAwesomeIcon className="cursor-pointer" icon={faCaretDown} />
           </div>
           <div className="flex space-x-4">
             {/* <input type="checkbox" className="h-5 w-8"/> */}
             <p>Status</p>
-            <FontAwesomeIcon icon={faCaretDown} />
+            <FontAwesomeIcon className="cursor-pointer" icon={faCaretDown} />
           </div>
         </div>
+
+
+        <div className="relative top-6 pl-4 pt-4 bg-white p-2.5 left-4 h-[11.5vh] rounded-xl space-x-20 flex">
+          <div className="flex space-x-4">
+          <img className="h-5 w-" src={Calendar}/>
+            <p>10 Dec, 2020</p>
+          
+          </div>
+          <div className="flex space-x-4">
+            {/* <input type="checkbox" className="h-5 w-8"/> */}
+            <p>MadLabs</p>
+          
+          </div>
+          <div className="flex space-x-4">
+            {/* <input type="checkbox" className="h-5 w-8"/> */}
+            <p>10</p>
+          
+          </div>
+          <div className="flex space-x-4 pl-12">
+            {/* <input type="checkbox" className="h-5 w-8"/> */}
+            <p>10</p>
+          
+          </div>
+          <div className="flex space-x-4 pl-16">
+            {/* <input type="checkbox" className="h-5 w-8"/> */}
+            <p>Atlas</p>
+          
+          </div>
+          <div className="flex space-x-4 pl-5">
+            {/* <input type="checkbox" className="h-5 w-8"/> */}
+            <p className="bg-[#EBF5ED] text-[#3A974C] px-5 py-2.5 h-11 relative -top-3 rounded-3xl">Complete</p>
+          
+          </div>
+        </div>
+
+        
         </div>
     </div>
   );
