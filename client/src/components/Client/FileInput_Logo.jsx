b34b34d1fca9ae9e170d03ae4946519999eabf1b
Emmanuel <nkememmanuel5505@gmail.com>
2024-02-14 20:26:07 +0100

    input_rightfull_placement

---
+++ client/src/components/Client/FileInput_Logo.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react';
+
+function FileInput_Logo() {
+  const [fileName, setFileName] = useState('Select a file');
+
+  const handleChange = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      setFileName(file.name);
+    } 
+  };
+
+  return (
+    <div className="relative w-36 rounded-full h-36 bg-[#FAFAFB]  shadow-sm">
+      <input
+        type="file"
+        id="myFile"
+        name="myFile"
+        className="absolute inset-0 w-full h-full opacity-0 cursor-pointer"
+        onChange={handleChange}
+      />
+      <label htmlFor="myFile" className="text-gray-700 cursor-pointer">
+        
+      </label>
+    </div>
+  );
+}
+
+export default FileInput_Logo;
--- client/src/components/Client/ProjectChooseSettings.jsx
+++ client/src/components/Client/ProjectChooseSettings.jsx
@@ -4,6 +4,8 @@ import Header from "../Header";
 import { useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
 import Camera from '../../images/Camera.png'
+import FileInput from "./FileInput_Logo";
+import FileInput_Cover from "./FileInput_Cover";
 
 function ProjectChooseSettings() {
   const [isOpen, setIsOpen] = useState(false);
@@ -35,13 +37,22 @@ function ProjectChooseSettings() {
             <span id="userName" className="text-uppercase"></span>
           </h3>
           <div className="relative top- space-x-4 left-12  flex">
-            <div className="relative top- space-x-4 left-12  flex">
+            <div className="flex space-x-12">
               <div className="justify-center text-center">
-                <p>Logo</p>
-                <div className="w-20 h-36 bg-[#FAFAFB]">
-                  <img src={Camera}/>
-                </div>
+                <p className="relative top-9">Logo</p>
+                  <img className="w-8 relative left-14 top-20 z-50" src={Camera}/>
+                <FileInput/>
+                {/* </input> */}
               </div>
+              <div className="justify-center text-center">
+                <p className="relative top-9">Cover Photo</p>
+                  <img className="w-8 relative left-32 top-20 z-50" src={Camera}/>
+                  <FileInput_Cover/>
+                {/* </input> */}
+              </div>
+             {/* <div>
+             <FileInput_Cover/>
+             </div> */}
             </div>
             <div className="relative top- space-x-4 left-12  flex"></div>
           </div>
