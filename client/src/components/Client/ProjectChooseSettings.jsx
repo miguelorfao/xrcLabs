1c736f355186b35cf18c718a08cc4871ec9ae144
Emmanuel <nkememmanuel5505@gmail.com>
2024-02-15 02:43:18 +0100

    right_side_start

--- client/src/components/Client/ProjectChooseSettings.jsx
+++ client/src/components/Client/ProjectChooseSettings.jsx
@@ -27,99 +27,153 @@ function ProjectChooseSettings() {
         </Header>
         <SideNav show={isOpen} />
 
-        <div
-          className={`min-h-screen overflow-x-hidden  left-[21%] w-[78.9%] pt-7 bg-[#E5E5E5] top-14 relative justify-content-center pb-40  ${
-            isOpen ? "opacity-70 z-20 bg-[#E5E5E5]" : ""
-          }`}
-        >
-          <h3 className="h-  relative -left-7 text-center font-semibold">
-            Project Settings{" "}
-            <span id="userName" className="text-uppercase"></span>
-          </h3>
-          <div className="relative top- space-x-4 left-12  flex">
-            <div className="h-[98%] w-[48%] left-[-1.3%]  top-1 rounded-sm bg-white relative mb-3 whitespace-nowrap p-10">
-              <div className="flex space-x-6">
-                <div className="justify-center text-center relative right-3">
-                  <p className="relative top-9">Logo</p>
-                  <img
-                    className="w-8 relative left-14 top-20 z-50"
-                    src={Camera}
-                  />
-                  <FileInput />
-                  {/* </input> */}
+        <div className="flex -space-x-44">
+          <div
+            className={`min-h-screen overflow-x-hidden  left-[21%] w-[78.9%] pt-7 bg-[#E5E5E5] top-14 relative justify-content-center pb-40  ${
+              isOpen ? "opacity-70 z-20 bg-[#E5E5E5]" : ""
+            }`}
+          >
+            <h3 className="h-  relative -left-7 text-center font-semibold">
+              Project Settings{" "}
+              <span id="userName" className="text-uppercase"></span>
+            </h3>
+            <div className="relative top- space-x-4 left-12  flex">
+              <div className="h-[98%] w-[48%] left-[-1.3%]  top-1 rounded-sm bg-white relative mb-3 whitespace-nowrap p-10">
+                <div className="flex space-x-6">
+                  <div className="justify-center text-center relative right-3">
+                    <p className="relative top-9">Logo</p>
+                    <img
+                      className="w-8 relative left-14 top-20 z-50"
+                      src={Camera}
+                    />
+                    <FileInput />
+                    {/* </input> */}
+                  </div>
+                  <div className="justify-center text-center relative right-2">
+                    <p className="relative top-9">Cover Photo</p>
+                    <img
+                      className="w-8 relative left-32 top-20 z-50"
+                      src={Camera}
+                    />
+                    <FileInput_Cover />
+                    {/* </input> */}
+                  </div>
                 </div>
-                <div className="justify-center text-center relative right-2">
-                  <p className="relative top-9">Cover Photo</p>
-                  <img
-                    className="w-8 relative left-32 top-20 z-50"
-                    src={Camera}
-                  />
-                  <FileInput_Cover />
-                  {/* </input> */}
-                </div>
-              </div>
-              <div className="mt-12 relative right-3">
-                <div className="flex">
-                  <div>
-                    <label className="text-black text-lg">Project Name</label>
+                <div className="mt-12 relative right-3">
+                  <div className="flex">
+                    <div>
+                      <label className="text-black text-lg">Project Name</label>
+                      <br />
+                      <input
+                        className="focus:outline-none placeholder:text-gray-600 bg-gray-100 px-3 rounded-lg py-2.5 mt-2.5"
+                        type="text"
+                        placeholder="My Project"
+                      />
+                    </div>
+                    <div className="ml-6">
+                      <label className="text-black text-lg">Mint Date</label>
+                      <br />
+                      <input
+                        className="pl-8 focus:outline-none placeholder:text-gray-600 bg-gray-100 px-[30%] rounded-lg py-2.5 mt-2.5"
+                        type="date"
+                        // placeholder="twitter.com/myproject"
+                      />
+                    </div>
+                  </div>
+                  <div className="flex mt-8">
+                    <div>
+                      <label className="text-black text-lg">Mint Price</label>
+                      <br />
+                      <input
+                        className="focus:outline-none placeholder:text-gray-600 bg-gray-100 px-3 rounded-lg py-2.5 mt-2.5"
+                        type="text"
+                        placeholder="0.5"
+                      />
+                    </div>
+                    <div className="ml-6">
+                      <label className="text-black text-lg">Chain</label>
+                      <br />
+                      <input
+                        className="focus:outline-none placeholder:text-gray-600 bg-gray-100 px-[11%] rounded-lg py-2.5 mt-2.5"
+                        type="text"
+                        placeholder="Solana"
+                      />
+                    </div>
+                  </div>
+                  <div className="mt-4">
+                    <label className="text-black text-lg">Template</label>
                     <br />
-                    <input
-                      className="focus:outline-none placeholder:text-gray-600 bg-gray-100 px-3 rounded-lg py-2.5 mt-2.5"
+                    <textarea
+                      className="focus:outline-none h-60 w-[104%] placeholder:text-gray-600 bg-gray-100 px-8 rounded-lg py-2.5 mt-2.5 pt-3"
                       type="text"
-                      placeholder="My Project"
-                    />
-                  </div>
-                  <div className="ml-6">
-                    <label className="text-black text-lg">Mint Date</label>
-                    <br />
-                    <input
-                      className="pl-8 focus:outline-none placeholder:text-gray-600 bg-gray-100 px-[30%] rounded-lg py-2.5 mt-2.5"
-                      type="date"
-                      // placeholder="twitter.com/myproject"
+                      placeholder="This will accept text, images & video format"
                     />
                   </div>
                 </div>
-                <div className="flex mt-8">
-                  <div>
-                    <label className="text-black text-lg">Mint Price</label>
-                    <br />
-                    <input
-                      className="focus:outline-none placeholder:text-gray-600 bg-gray-100 px-3 rounded-lg py-2.5 mt-2.5"
-                      type="text"
-                      placeholder="0.5"
-                    />
-                  </div>
-                  <div className="ml-6">
-                    <label className="text-black text-lg">Chain</label>
-                    <br />
-                    <input
-                      className="focus:outline-none placeholder:text-gray-600 bg-gray-100 px-[11%] rounded-lg py-2.5 mt-2.5"
-                      type="text"
-                      placeholder="Solana"
-                    />
-                  </div>
-                </div>
-                <div className="mt-4">
-                  <label className="text-black text-lg">Template</label>
-                  <br />
-                  <textarea
-                    className="focus:outline-none h-60 w-[104%] placeholder:text-gray-600 bg-gray-100 px-8 rounded-lg py-2.5 mt-2.5 pt-3"
-                    type="text"
-                    placeholder="This will accept text, images & video format"
-                  />
-                </div>
-              </div>
 
-              <button
-                data-bs-toggle="modal"
-                data-bs-target="#popup"
-                className="px-11 py-2 relative left-[68.5%] mt-14 text-white rounded-lg bg-[#605BFF]"
-              >
-                Save Settings
-              </button>
-              {/* <button onClick={openPopup} className="px-7 py-2 relative left-[33%] mt-14 text-white rounded-lg bg-[#605BFF]">Request Whitelist</button> */}
+                <button
+                  data-bs-toggle="modal"
+                  data-bs-target="#popup"
+                  className="px-11 py-2 relative left-[68.5%] mt-14 text-white rounded-lg bg-[#605BFF]"
+                >
+                  Save Settings
+                </button>
+                {/* <button onClick={openPopup} className="px-7 py-2 relative left-[33%] mt-14 text-white rounded-lg bg-[#605BFF]">Request Whitelist</button> */}
+              </div>
+              <div className="relative top- space-x-4 left-12  flex"></div>
             </div>
-            <div className="relative top- space-x-4 left-12  flex"></div>
+          </div>
+
+          <div className="h-96 w-[36.5%] right-9 top-32 rounded-sm bg-white relative whitespace-nowrap p-9">
+            <div className="flex">
+              <div>
+                <label className="text-black text-lg">Project Name</label>
+                <br />
+                <input
+                  className="focus:outline-none placeholder:text-gray-600 bg-gray-100 px-3 rounded-lg py-2.5 mt-2.5"
+                  type="text"
+                  placeholder="Name"
+                />
+              </div>
+              <div className="ml-4">
+                <label className="text-black text-lg">Twitter</label>
+                <br />
+                <input
+                  className="focus:outline-none placeholder:text-gray-600 bg-gray-100 px-3 rounded-lg py-2.5 mt-2.5"
+                  type="text"
+                  placeholder="twitter.com/myproject"
+                />
+              </div>
+            </div>
+            <div className="flex mt-8">
+              <div>
+                <label className="text-black text-lg">Website</label>
+                <br />
+                <input
+                  className="focus:outline-none placeholder:text-gray-600 bg-gray-100 px-3 rounded-lg py-2.5 mt-2.5"
+                  type="text"
+                  placeholder="myproject.com"
+                />
+              </div>
+              <div className="ml-4">
+                <label className="text-black text-lg">Discord</label>
+                <br />
+                <input
+                  className="focus:outline-none placeholder:text-gray-600 bg-gray-100 px-3 rounded-lg py-2.5 mt-2.5"
+                  type="text"
+                  placeholder="discord.com"
+                />
+              </div>
+            </div>
+
+            <button
+              data-bs-toggle="modal"
+              data-bs-target="#popup"
+              className="px-7 py-2 relative left-[33%] mt-14 text-white rounded-lg bg-[#605BFF]"
+            >
+              Request Whitelist
+            </button>
+            {/* <button onClick={openPopup} className="px-7 py-2 relative left-[33%] mt-14 text-white rounded-lg bg-[#605BFF]">Request Whitelist</button> */}
           </div>
         </div>
       </div>
